cd58df85ec92806930ed5d0183876e0954bb71ae
luciferwqy <190873411@163.com>
2017-07-16 01:00:03 +0800

    commit 支付宝notify是post请求，不需要转码

--- src/main/java/com/qingdao/marathon/pay/controller/PayController.java
+++ src/main/java/com/qingdao/marathon/pay/controller/PayController.java
@@ -267,7 +267,7 @@ public class PayController {
 							: valueStr + values[i] + ",";
 				}
 				//乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
-				valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+//				valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
 				sysLogger.info(LoggerConstants.PAY, "回调信息=====name="+name + "&&&&&& valueStr="+valueStr);
 				params.put(name, valueStr);
 			}
--- src/main/java/com/qingdao/marathon/pay/wxpay/config/WXConfig.java
+++ src/main/java/com/qingdao/marathon/pay/wxpay/config/WXConfig.java
@@ -1,5 +1,7 @@
 package com.qingdao.marathon.pay.wxpay.config;
 
+import com.qingdao.marathon.util.URLUtils;
+
 public class WXConfig {
 
 	public static final String APP_ID = "wx694dd1af2b95c3a4";
@@ -8,11 +10,11 @@ public class WXConfig {
 	
 	public static final String API_KEY = "mnbvcxzlkjhgfdsapoiuytrewq032701";
 	//发起电脑IP
-	public static final String CREATE_IP = "36.22.63.94";
+	public static final String CREATE_IP = "120.27.225.133";
 	
 //	public static final String CREATE_IP = "121.196.229.214";
 	
 	public static final String URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 	
-	public static final String NOTIFY_URL = "http://121.196.229.214:8080/marathon/payMng/wxPayReturn.shtml";
+	public static final String NOTIFY_URL = URLUtils.get("marathon")+"/payMng/wxPayReturn.shtml";
 }
